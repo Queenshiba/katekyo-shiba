e5b5d209a282376d3cfe742cd8ae1c8b0e425999
Queenshiba <haruka0111.2a@gmail.com>
2021-07-20 16:05:14 +0900

    update email

--- dist/main.js
+++ dist/main.js
@@ -4,3 +4,17 @@ images.forEach(image => {
     const newURL = image.getAttribute('data-src')
     image.src = newURL;
 })
+
+
+let copyEmailText = document.getElementById('copyEmailText')
+let copyEmailBtn = document.getElementById('copyEmailBtn')
+
+copyEmailBtn.addEventListener("click",() =>copyEmail())
+
+function copyEmail() {
+    let copyEmailText = document.getElementById('copyEmailText')
+    copyEmailText.select();
+    copyEmailText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+
+}
